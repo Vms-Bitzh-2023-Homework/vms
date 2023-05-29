3be517751c4acaa658b324bee47e19ccb62f0c21
xx <937815432@qq,com>
2023-05-29 09:00:26 +0800

    定义一个常量类

---
+++ src/main/java/com/zhbit/common/Constant.java
@@ -0,0 +1,10 @@
+package com.zhbit.common;
+
+public class Constant {
+
+    public static final String admin = "\"admin\"";
+    public static final String status1 = "未付款";
+    public static final String status2 = "已付款";
+
+
+}
--- src/main/java/com/zhbit/common/Perms.java
+++
@@ -1,8 +0,0 @@
-package com.zhbit.common;
-
-public class Perms {
-
-    public static final String admin = "\"admin\"";
-    public static final String user = "\"user\"";
-
-}
